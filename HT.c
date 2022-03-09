87ab218d60d4dd1445aada81e2b150cbfcff6894
Maximilián Strečanský <98816862+xstrecansky@users.noreply.github.com>
2022-03-09 17:06:49 +0100

    Add files via upload

--- HT.c
+++ HT.c
@@ -1,34 +1,74 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-#define SIZE 10
-struct hash* hashTable[SIZE];
-
-struct hash{
-    int data;
-    int key;
-};
-//Funkcia na pridanie hodnoty do tabulky
-void insert(int key, int data){
-
+//Funkcia na vratenie hodnoty
+int returnKey(int data, int size){
+    return data%(size);
 }
-//Funkcia na vypisanie hasovacej tabulky
-void printHashTable(){
-    for(int i=0;i<SIZE;i++){
-        if(hashTable[i]!=NULL)
-            printf(" (%d,%d)",hashTable[i]->key,hashTable[i]->data);
+void initHashTable(int size, int *hashTable){
+    for(int i=0;i<size;i++){
+        (hashTable)[i]=0;
     }
 }
-//Funkcia na vratenie hodnoty
-int returnKey(int key){
-    return key%SIZE;
+//Funkcia na vypisanie hodnot v tabulke
+void printHashTable(int size, int *hashTable){
+    for(int i=0;i<size;i++){
+        if((hashTable)[i]==0)
+            printf("%d\t---\n",i);
+        else
+            printf("%d\t%d\n",i,hashTable[i]);
+    }
+}
+//Funkcia na vlozenie prvku do hashovacej tabulky
+void insert(int size , int *hashTable, int data, int key){
+    if(hashTable[key]==0)
+        hashTable[key]=data;
+    else{
+        key++;
+        if(key>=(size))
+            key=0;
+        insert(size, hashTable, data, (key));
+    }
+    return;
+}
+//Funkcia na vyhladanie prvku v tabulke
+int search(int size, int *hashTable, int data, int key){
+    if(hashTable[key]==0)
+        return 0;
+    if(hashTable[key]==data)
+        return 1;
+    else{
+        key++;
+        if(key>=(size))
+            key=0;
+        search(size, hashTable, data, key);
+    }
+    return 1;
+}
+//funkcia na vymazanie prvku v tabulke
+void delete(int size, int *hashTable, int data, int key){
+    if(hashTable[key]==0)
+        return;
+    if(hashTable[key]==data){
+        hashTable[key]=0;
+        return;
+    }
+    else{
+        key++;
+        if(key>=size)
+            key=0;
+        delete(size, hashTable, data, key);
+    }
+    return;
 }
 int main(){
     int i=0,j=0;
     int initValue, inputValue;
     printf("Vloz hodnotu do hasovacej tabulky\n");
     scanf("%d",&initValue);
-    //insert();
+    int size=100;
+    int hashTable[size];
+    initHashTable(size, (hashTable));
+    insert(size, hashTable, initValue, returnKey(initValue, size));
     while(i==0){
         printf("Vloz dalsiu hodnotu do hasovacej tabulky\nAk vlozis %d, program skonci\n",initValue);
         scanf("%d",&inputValue);
@@ -38,13 +78,18 @@ int main(){
                 scanf("%d",&inputValue);
                 switch (inputValue){
                     case 1:
-                        //vypiseme hodnoty
+                        printHashTable(size, hashTable);
                         break;
                     case 2:
-                        //vymazeme hodnoty
+                        scanf("%d",&inputValue);
+                        delete(size, hashTable, inputValue, returnKey(inputValue, size));
                         break;
                     case 3:
-                        //najdeme hodnotu;
+                        scanf("%d",&inputValue);
+                        if(search(size, hashTable, inputValue, returnKey(inputValue, size)))
+                            printf("Hodnota %d sa nachadza v tabulke\n", inputValue);
+                        else
+                            printf("Hodnota %d sa nenachadza v tabulke\n", inputValue);
                         break;
                     case 4:
                         j=1;
@@ -55,6 +100,7 @@ int main(){
                         break;
                 }
             }
+        insert(size,hashTable,inputValue, returnKey(inputValue, size));
     }
     return 0;
 }
